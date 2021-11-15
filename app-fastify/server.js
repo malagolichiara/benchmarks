03fd14d07e2276dbf2dc977ce8a5832613444ed4
Malagoli Chiara <malagoli@gbinside.com>
2021-11-15 19:05:29 +0000

    add a small NODEJS implementation, uses cluster

--- app-fastify/server.js
+++ app-fastify/server.js
@@ -1,35 +1,51 @@
-// Require the framework and instantiate it
+const cluster = require('cluster');
+const numCPUs = require('os').cpus().length;
 const fastify = require('fastify')({ logger: false })
 fastify.register(require('fastify-formbody'))
+const port = 8000;
 
 var values = {}
 
-// Declare a route
-fastify.get('/', async (request, reply) => {
-  return { hello: 'world' }
-})
-
-fastify.get('/items/:idx', async (request, reply) => {
-  return {
-    value: values[request.params.idx]
+if (cluster.isMaster) {
+  console.log(`Master ${process.pid} is running`);
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork();
   }
-})
+  cluster.on('exit', worker => {
+    console.log(`Worker ${worker.process.pid} died`);
+  });
+} else {
+  // Declare a route
+  fastify.get('/', async (request, reply) => {
+    return { hello: 'world' }
+  })
 
-fastify.post('/items/:idx', async (request, reply) => {
-  values[request.params.idx] = request.body.value
-  return {
-    value: values[request.params.idx]
-  }
-})
+  fastify.get('/items/:idx', async (request, reply) => {
+    return {
+      value: values[request.params.idx]
+    }
+  })
 
-// Run the server!
-const start = async () => {
-  try {
-    await fastify.listen(8000)
-  } catch (err) {
-    fastify.log.error(err)
-    process.exit(1)
-  }
+  fastify.post('/items/:idx', async (request, reply) => {
+    values[request.params.idx] = request.body.value
+    return {
+      value: values[request.params.idx]
+    }
+  })
+
+  fastify.listen(port, () => {
+    console.log(`Fastify listening on port ${port}, PID: ${process.pid}`);
+  });
+
+  // Run the server!
+  // const start = async () => {
+  //   try {
+  //     await fastify.listen(8000)
+  //   } catch (err) {
+  //     fastify.log.error(err)
+  //     process.exit(1)
+  //   }
+  // }
+
+  // start()
 }
-
-start()
